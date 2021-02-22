1bc94ec43838605daf9ec25b457a65b8debd7de4
Joseph Nielsen <josephmnielsen@gmail.com>
2021-02-21 19:21:51 -0800

    update functioning planner

--- Develop/script.js
+++ Develop/script.js
@@ -3,24 +3,91 @@
 // Create a function to change background color of text-area depending on hour
 
 
-document.getElementById('currentDay').textContent = moment().format('MMMM Do YYYY, h:mm:ss a')
+document.getElementById('currentDay').textContent = moment().format('MMMM Do YYYY, h:mm a')
 
-let arr = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm']
-let currentTime = moment().format('LT')
-console.log(currentTime)
-let agendaTime = document.getElementById('timeOfDay').textContent
-console.log(agendaTime)
+var currentHour = new Date()
+var hours = currentHour.getHours()
 
 
-  if (agendaTime < currentTime ) {
-    document.getElementById('agenda').className.add('past')
+ for (i = 0; i <24; i++)
 
-  } else if (agendaTime = currentTime ) {
-    document.getElementById('agenda').className.add('present')
-
-  } else if (agendaTime > currentTime) {
-    document.getElementById('agenda').className.add('future')
-  
-  }
-
-
+ if (hours > 9) {
+   document.getElementById('nine').classList.add('past')
+ } else if (hours < 9) {
+   document.getElementById('nine').classList.add('future')
+ }
+  else if (hours == '9') {
+   document.getElementById('nine').classList.add('present')
+}
+if (hours > 10) {
+  document.getElementById('ten').classList.add('past')
+}
+else if (hours < 10) {
+  document.getElementById('ten').classList.add('future')
+}
+else if (hours == '10') {
+  document.getElementById('ten').classList.add('present')
+}
+if (hours > 11) {
+  document.getElementById('eleven').classList.add('past')
+}
+else if (hours < 11) {
+  document.getElementById('eleven').classList.add('future')
+}
+else if (hours == '11') {
+  document.getElementById('eleven').classList.add('present')
+}
+if (hours > 12) {
+  document.getElementById('twelve').classList.add('past')
+}
+else if (hours < 12) {
+  document.getElementById('twelve').classList.add('future')
+}
+else if (hours == '12') {
+  document.getElementById('twelve').classList.add('present')
+}
+if (hours > 13) {
+  document.getElementById('one').classList.add('past')
+}
+else if (hours < 13) {
+  document.getElementById('one').classList.add('future')
+}
+else if (hours == '13') {
+  document.getElementById('one').classList.add('present')
+}
+if (hours > 14) {
+  document.getElementById('two').classList.add('past')
+}
+else if (hours < 14) {
+  document.getElementById('two').classList.add('future')
+}
+else if (hours == '14') {
+  document.getElementById('two').classList.add('present')
+}
+if (hours > 15) {
+  document.getElementById('three').classList.add('past')
+}
+else if (hours < 15) {
+  document.getElementById('three').classList.add('future')
+}
+else if (hours == '15') {
+  document.getElementById('three').classList.add('present')
+}
+if (hours > 16) {
+  document.getElementById('four').classList.add('past')
+}
+else if (hours < 16) {
+  document.getElementById('four').classList.add('future')
+}
+else if (hours == '16') {
+  document.getElementById('four').classList.add('present')
+}
+if (hours > 17) {
+  document.getElementById('five').classList.add('past')
+}
+else if (hours < 17) {
+  document.getElementById('five').classList.add('future')
+}
+else if (hours == '17') {
+  document.getElementById('five').classList.add('present')
+}
